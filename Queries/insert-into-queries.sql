ea693fa16dacf1d8042a3dadef27cb542b161715
Lera <lerakachanovskaya@gmail.com>
2023-10-19 20:32:32 +0300

    Insert values into user table

---
+++ Queries/insert-into-queries.sql
@@ -0,0 +1,10 @@
+INSERT INTO "user" (name, surname, password, email, issuperuser,
+				   isstaff)
+VALUES 	('admin', 'admin', 'adminPass', 'admin@mail.ru', true, false),
+		('Виктория', 'Звягинцева', '1234', 'vika@gmail.com', false, false),
+	    ('Евгений', 'Кахновский', 'password', 'kaxxa2927@gmail.com',false, false),
+		('Владислав', 'Плиска', 'newPass', 'email@gmail.com' , false, true),
+		('Анна', 'Жвакина', 'pass123', 'anna@gmail.com', false, true),
+		('Игорь', 'Гламаздин', 'anotherPass', 'csharpovich@gmail.com', false, true),
+		('Вячеслав', 'Проволоцкий', 'qwerty', 'v.provolotskiy@gmail.com', false, true);
+		
