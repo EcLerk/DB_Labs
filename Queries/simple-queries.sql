a3d709e231481fae05d5e73ff3c372e965cf1270
Lera <lerakachanovskaya@gmail.com>
2023-10-23 21:56:27 +0300

    Add queries to simple-queries.sql

--- Queries/simple-queries.sql
+++ Queries/simple-queries.sql
@@ -1,11 +1,30 @@
-SELECT * FROM "user"
+SELECT * FROM "user";
 
-SELECT name, surname FROM "user"
+SELECT name, surname FROM "user";
 
-SELECT MAX(grade) FROM grade
+SELECT MAX(grade) FROM grade;
 
-SELECT MIN(grade) FROM grade
+SELECT MIN(grade) FROM grade;
 
 SELECT building, COUNT(number) AS "classrooms count"
 FROM classroom
-GROUP BY building
+GROUP BY building;
+
+SELECT * FROM discipline
+ORDER BY name;
+
+SELECT * FROM "user"
+ORDER BY surname
+LIMIT 5 OFFSET 2;
+
+SELECT * FROM classroom
+WHERE number BETWEEN 200 AND 300;
+
+SELECT * FROM "user"
+WHERE email NOT LIKE '%mail.ru';
+
+SELECT studentId, AVG(grade)
+FROM grade
+GROUP BY studentId
+HAVING AVG(grade) > 8
+ORDER BY studentId DESC;
